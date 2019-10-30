2c5f80bf01d1269d36f4d61063e791775a53ffff
trong <trong@iastate.edu>
2019-10-29 22:59:19 -0500

    hello world

---
+++ Minesweeper/Cell.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for Cell.xaml
+    /// </summary>
+    public partial class Cell : UserControl
+    {
+        public enum CellType { Unknow, Flag, Mine, Open, GameOver }
+
+        public event EventHandler Open;
+
+        public Cell(int x, int y)
+        {
+            InitializeComponent();
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        private bool isFlag;
+        public bool IsFlag
+        {
+            get
+            {
+                return isFlag;
+            }
+            set
+            {
+                isFlag = value;
+                DotLabel.Visibility = value && Type == CellType.Unknow
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+            }
+        }
+
+        private int? value;
+        public int? Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+                Type = CellType.Open;
+            }
+        }
+
+        public bool IsMine { get; set; }
+
+        private CellType type = CellType.Unknow;
+        public CellType Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                switch (value)
+                {
+                    case CellType.Unknow:
+                        DotLabel.Visibility = IsFlag ? Visibility.Visible : Visibility.Hidden;
+                        Flag.Visibility = Visibility.Hidden;
+                        break;
+                    case CellType.Flag:
+                        DotLabel.Visibility = Visibility.Hidden;
+                        Flag.Visibility = Visibility.Visible;
+                        break;
+                    case CellType.Mine:
+                        DotLabel.Visibility = Visibility.Hidden;
+                        if (Flag.Visibility != Visibility.Visible)
+                            Mine.Visibility = Visibility.Visible;
+                        break;
+                    case CellType.Open:
+                        DotLabel.Visibility = Visibility.Hidden;
+                        Flag.Visibility = Visibility.Hidden;
+                        ValueLabel.Content = Value == 0 ? "" : Value.ToString();
+                        cell.Background = Brushes.LightGray;
+                        break;
+                }
+            }
+        }
+
+        internal void GameOver(object sender, EventArgs e)
+        {
+            Type = CellType.GameOver;
+            if (Flag.Visibility == Visibility.Visible && !IsMine)
+            {
+                ValueLabel.Content = "X";
+            }
+            if (Flag.Visibility == Visibility.Hidden && IsMine)
+            {
+                Type = CellType.Mine;
+            }
+        }
+
+        private void Click(object sender, MouseButtonEventArgs e)
+        {
+            switch (Type)
+            {
+                case CellType.Unknow:
+                    if (IsFlag == (e.ChangedButton == MouseButton.Left))
+                        SetFlag();
+                    else
+                        Open?.Invoke(this, e);
+                    break;
+                case CellType.Flag:
+                    SetFlag();
+                    break;
+                case CellType.Open:
+                    Open?.Invoke(this, e);
+                    break;
+            }
+        }
+
+        private void SetFlag()
+        {
+            if (Type == CellType.Unknow) Type = CellType.Flag;
+            else if (Type == CellType.Flag) Type = CellType.Unknow;
+        }
+    }
+}
---
+++ Minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public event EventHandler GameOver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void Reset(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            if (board == null || NumOfMines == null || width == null || hight == null) return;
+            int x = width.Value.Value;
+            int y = hight.Value.Value;
+            int numOfCells = x * y;
+            board.Children.Clear();
+            for (int i = 0; i < width.Value.Value; i++)
+            {
+                StackPanel stackPanel = new StackPanel();
+                board.Children.Add(stackPanel);
+                for (int j = 0; j < hight.Value.Value; j++)
+                {
+                    Cell cell = new Cell(i, j);
+                    stackPanel.Children.Add(cell);
+                    this.GameOver += cell.GameOver;
+                    FlagToggle.Checked += (object sender, RoutedEventArgs e) => { cell.IsFlag = true; };
+                    FlagToggle.Unchecked += (object sender, RoutedEventArgs e) => { cell.IsFlag = false; };
+                    cell.Open += (object sender, EventArgs e) => { Open((Cell)sender); };
+                }
+            }
+
+            int count = NumOfMines.Value.Value;
+            Random random = new Random();
+            while (count > 0)
+            {
+                int i = random.Next(1, x);
+                int j = random.Next(1, y);
+                Cell cell = GetCell(i, j);
+                if (!cell.IsMine)
+                {
+                    cell.IsMine = true;
+                    count--;
+                }
+            }
+        }
+
+        private Cell GetCell(int x, int y)
+        {
+            if (x < 0 || x >= width.Value.Value || y < 0 || y >= hight.Value.Value) return null;
+            return (Cell)((StackPanel)board.Children[x]).Children[y];
+        }
+
+        private void Open(int x, int y)
+        {
+            Cell cell = GetCell(x, y);
+            if (cell == null || cell.Type != Cell.CellType.Unknow) return;
+            Open(cell);
+        }
+
+        private void Open(Cell cell)
+        {
+            if (cell.IsMine)
+            {
+                // Game over
+                FlagToggle.IsChecked = false;
+                GameOver?.Invoke(this, null);
+                MessageBox.Show("Restart", "Game Over", MessageBoxButton.OK);
+                Reset();
+                return;
+            }
+
+            switch (cell.Type)
+            {
+                case Cell.CellType.Unknow:
+                    cell.Value = Value(cell);
+                    if (cell.Value == 0)
+                    {
+                        // expand
+                        for (int i = cell.X - 1; i <= cell.X + 1; i++)
+                            for (int j = cell.Y - 1; j <= cell.Y + 1; j++)
+                                Open(i, j);
+                    }
+                    break;
+                case Cell.CellType.Open:
+                    if (cell.Value <= CountFlag(cell))
+                    {
+                        // Auto open
+                        for (int i = cell.X - 1; i <= cell.X + 1; i++)
+                            for (int j = cell.Y - 1; j <= cell.Y + 1; j++)
+                                if (GetCell(i, j) != null && GetCell(i, j).Type == Cell.CellType.Unknow)
+                                    Open(i, j);
+                    }
+                    break;
+            }
+        }
+
+        private int Value(Cell cell)
+        {
+            int c = 0;
+            for (int i = cell.X - 1; i <= cell.X + 1; i++)
+                for (int j = cell.Y - 1; j <= cell.Y + 1; j++)
+                    c += GetCell(i, j) != null && GetCell(i, j).IsMine ? 1 : 0;
+            return c;
+        }
+
+        private int CountFlag(Cell cell)
+        {
+            int c = 0;
+            for (int i = cell.X - 1; i <= cell.X + 1; i++)
+                for (int j = cell.Y - 1; j <= cell.Y + 1; j++)
+                    c += GetCell(i, j) != null && GetCell(i, j).Type == Cell.CellType.Flag ? 1 : 0;
+            return c;
+        }
+    }
+}
